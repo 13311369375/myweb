7c869df3b7f668b3ff43a7252b71fa8ad21c02c3
颜冰 <yanbing@ty.chinatelcon.cn>
2020-06-24 09:58:46 +0800

    测试git

--- myweb/views.py
+++ myweb/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render,HttpResponse
 from myapp import models
+
+# 测试git
 def add_data(request):
     data = models.reg_mobile(meid="A10000000000000000",brand='荣耀',seller="京东",data_num="2008-8-8")
     data.save()
